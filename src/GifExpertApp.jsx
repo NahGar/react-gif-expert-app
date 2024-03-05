03d3b3b24045d54989c3d9b9088726886de4dfe2
Nahuel Garcia <nhlgrc@hotmail.com>
2024-03-05 13:40:55 -0300

    first commit

---
+++ src/GifExpertApp.jsx
@@ -0,0 +1,53 @@
+import { useState } from "react";
+import { AddCategory } from "./components/AddCategory";
+
+let contador = 1;
+
+export const GifExpertApp = () => {
+
+    const [categories, setCategories] = useState([ '' ]);
+
+    
+    const onAddCategory = ( newCategory ) => {
+        
+        //Forma 1 de hacerlo. Spread (...) convierte en una lista de items para 
+        //simplificar agregar otros items
+        //setCategories( [ ...categories, 'Probando ' + contador] );
+        //Forma 2 de hacerlo
+        //setCategories( cat => [ ...cat, 'Probando ' + contador] );
+        //contador += 1;
+
+        //2 formas
+        //if( categories.indexOf( newCategory ) !== -1) return;
+
+        const categoriesLower = categories.map(element => {
+            return element.toLowerCase();
+        });
+
+        if( categoriesLower.includes( newCategory.toLowerCase() ) ) return;
+
+        setCategories( [ newCategory, ...categories ] );
+
+    }
+
+    return (
+        <>
+            <h1>GifExpertApp</h1>
+
+            <AddCategory 
+                //setCategories={ setCategories }
+                onNewCategory={ ( value ) => onAddCategory( value ) }
+            />
+
+            <ol>
+                { //if(categories.length > 0 ) {
+                     categories.map( category => {
+                            return <li key={ category }>{category}</li>
+                        }) 
+                  //}
+                    
+                }
+            </ol>
+        </>
+    )
+}
---
+++ src/components/AddCategory.jsx
@@ -0,0 +1,38 @@
+import { useState } from "react"
+
+
+//export const AddCategory = ( { setCategories} ) => {
+export const AddCategory = ( { onNewCategory } ) => {
+
+    const [inputValue, setInputValue] = useState('');
+
+    const onInputChange = (event) => {
+      setInputValue( event.target.value );
+    }
+
+    const onSubmit = (event) => {
+      event.preventDefault();
+
+      if( inputValue.trim().length <= 1 ) return;
+      //if( categories.findIndex( inputValue.trim() ) !== undefined) return;
+
+      //hace callback ya que categories no está definido aquí
+      //setCategories( categories => [ inputValue, ...categories] );
+
+      //devuelve a padre mediante prop onNewCategory
+      onNewCategory( inputValue.trim() );
+
+      setInputValue('');
+    }
+
+    return (
+        <form onSubmit={ onSubmit }>
+          <input 
+              type = "text"
+              placeholder= "Buscar gifs"
+              value={ inputValue }
+              onChange={ onInputChange }
+          />
+        </form>
+    )
+}
